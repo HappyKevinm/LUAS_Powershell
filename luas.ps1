f44c62c184ad5d73cdcbd290a0a74be90617c626
Kevin Miller <kevinm@wlkmmas.org>
2018-01-14 10:46:50 +0000

    Update luas.ps1

--- luas.ps1
+++ luas.ps1
@@ -1,3 +1,57 @@
+function Get-LuasTimesScrape
+{
+<#
+.SYNOPSIS
+    Makes a web call to https://www.luas.ie/index.php webpage and scrapes times
+ 
+.DESCRIPTION
+    Makes a web call to https://www.luas.ie/index.php webpage and scrapes times based on JSON stop list I stuck in GIT
+    https://raw.githubusercontent.com/HappyKevinm/LUAS_Powershell/master/LuasStops.json" 
+ 
+.PARAMETER LuasStop
+    LUAS stop name needs to match correct formatting - you can tab then out to get the spelling 
+
+.PARAMETER LuasDirection  
+    Directions needs to be Inbound or Outbound towards the city center seems to be the logic
+
+ .EXAMPLE
+    Get times for The Gallops Stop Headed Inbound towards city center
+        Get-LuasTimesScrape -LuasStop TheGallops -LuasDirection Inbound
+
+        
+.NOTES
+    Author:  Kevin Miller
+    Website: http://www.happymillfam.com
+    Email: kevinm@wlkmmas.org
+#>
+  
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)][validateset("Cabra","Phibsborough","Grangegorman","BroadstoneDIT","Dominick","Parnell","Marlborough","Trinity","OConnellUpper","OConnellGPO","Westmoreland","Dawson","StStephensGreen","Harcourt","Charlemont","Ranelagh","Beechwood","Cowper","Milltown","WindyArbour","Dundru","Balally","Kilmacud","Stillorgan","Sandyford","CentralPark","Glencairn","TheGallops","LeopardstownValley","BallyoganWood","Carrickmines","Laughanstown","Cherrywood","BridesGlen","Tallaght","Hospital","Cookstown","Saggart","Fortunestown","CitywestCampus","Cheevertown","Fettercairn","Belgard","Kingswood","RedCow","Kylemore","Bluebell","Blackhorse","Drimnagh","Goldenbridge","SuirRoad","Rialto","Fatima","Jamess","Heuston","Museum","Smithfield","FourCourts","Jervis","AbbeyStreet","Busaras","Connolly","GeorgesDock","MayorSqureNCI","SpencerDock","ThePoint")][string]$LuasStop,
+    [Parameter(Mandatory=$True)][validateset("Inbound","Outbound")][string]$LuasDirection
+)
+
+
+process
+    {
+        If (!$LuasStops)
+        {
+            Write-host -ForegroundColor Red "We don't have the Stop list. Optaining it from GIT - https://raw.githubusercontent.com/HappyKevinm/LUAS_Powershell/master/LuasStops.json "
+            $LuasStops = Invoke-WebRequest -uri "https://raw.githubusercontent.com/HappyKevinm/LUAS_Powershell/master/LuasStops.json" | convertfrom-json 
+        }
+        $LuasStopURI = $LuasStops.Where({$_.Shortname -eq $LuasStop}).StopUriName
+        write-host -ForegroundColor green "Looking up LUAS times for $($LuasStop) stops for trains going $($LuasDirection)"
+        write-host -ForegroundColor magenta "URI used = https://www.luas.ie/index.php?id=346&get=$($LuasStopURI)&direction=$($LuasDirection)"
+        $LuasData = Invoke-WebRequest -uri "https://www.luas.ie/index.php?id=346&get=$($LuasStopURI)&direction=$($LuasDirection)"
+        $results = ($LuasData.ParsedHtml.childNodes | select innerText).innertext
+        if (!$Results)
+        {
+            $results = "No Results returned. Sorry this happens from time to time - Try again"
+        }
+    return $results 
+    }
+} 
 function Get-LuasTimes
 {
 <#
